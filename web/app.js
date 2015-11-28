db799bf1acfbc8de92ad747f056774335d35166d
y-ohgi <ohgi.211@gmail.com>
2015-11-28 18:37:56 +0900

    express版へ変更

---
+++ web/app.js
@@ -0,0 +1,21 @@
+var path = require('path');
+var express = require('express');
+var multer  = require('multer');
+var fs = require('fs');
+
+var app = express();
+
+var upload = multer({ dest: 'uploads/' });
+app.use(express.static(path.join(__dirname, 'public_html')));
+
+app.get('/hoge-', function (req, res) {
+    res.send('Hello World!');
+});
+
+
+var server = app.listen(3000, function () {
+    var host = server.address().address;
+    var port = server.address().port;
+
+    console.log('Example app listening at http://%s:%s', host, port);
+});
